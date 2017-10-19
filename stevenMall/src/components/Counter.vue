1ed3bd6a8a54e3a8d9be85faf126ce2bed452048
steven780122 <steven780122@yahoo.com.tw>
2017-10-19 14:01:53 +0800

    Add component

---
+++ stevenMall/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="increment">+</button>
+        <button v-on:click="decrement">-</button>       
+        <!-- v-on也可以 -->
+        <p><span>{{num}}</span></p>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            num: 0,
+        }
+    },
+    methods:{
+        increment(){
+            this.num++;
+
+        },
+        decrement(){
+            this.num--;
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
